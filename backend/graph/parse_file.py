80e5e25b41e239e7adce90c711e1d9e811ae35c8
koo-arch <yukshw.ko.0623@gmail.com>
2024-05-01 23:32:35 +0900

    Sanitize file

--- backend/graph/parse_file.py
+++ backend/graph/parse_file.py
@@ -1,4 +1,6 @@
 import os
+from django.utils.html import escape
+from .sanitize import Sanitize
 
 class ParseTextFile:
     def __init__(self, files: list, registered_data: dict):
@@ -15,19 +17,16 @@ class ParseTextFile:
             "ImV3omega(V)",
         ]
 
-
     def add_error(self, error_dict, file_name, message) -> None:
         if file_name not in error_dict:
             error_dict[file_name] = []
         error_dict[file_name].append(message)
-    
 
     def set_errors(self, error_response, error_dict) -> None:
         for name, errors in error_dict.items():
             if name not in error_response:
                 error_response[name] = []
             error_response[name].extend(errors)
-    
 
     def parse_errors(self) -> dict:
         if self.value_errors or self.column_errors:
@@ -37,7 +36,6 @@ class ParseTextFile:
 
             return error_response
 
-
     def process_measurement_data(self, values, columns_name, index, file) -> None:
         for i, column in enumerate(columns_name):
             try:
@@ -46,10 +44,8 @@ class ParseTextFile:
                 self.add_error(self.value_errors, file.name, f"{index}行目の{column}が数値ではありません。")
                 values[i] = None
 
-
     def validate_columns(self, columns_name: list) -> bool:
         return columns_name == self.default_columns_name
-
 
     def parse_text_file(self) -> dict:
         file_data = self.registered_data
@@ -58,19 +54,20 @@ class ParseTextFile:
 
         for file in files:
             lines = file.read().decode("utf-8").splitlines()
+            sanitized_file_name = Sanitize.sanitize_filename(file.name)
 
             start_processing = False
             measurement_data = []
 
             for index, line in enumerate(lines, 1):
 
-                # Coulmns>> からデータ処理を開始する
+                # Columns>> からデータ処理を開始する
                 if line.startswith("Columns>>"):
                     columns = line.split("Columns>> ")[1].strip().split()
-                    columns_name = [column for column in columns]
+                    columns_name = [escape(column) for column in columns]
 
                     if not self.validate_columns(columns_name):
-                        self.add_error(column_errors, file.name, "Columns>> で指定された列名が不正です。")
+                        self.add_error(column_errors, sanitized_file_name, "Columns>> で指定された列名が不正です。")
                         break
 
                     start_processing = True
@@ -84,9 +81,9 @@ class ParseTextFile:
                         measurement_data.append(dict(zip(columns_name, values)))
 
             if not start_processing and not column_errors:
-                self.add_error(column_errors, file.name, "Columns>> が見つかりません。")
+                self.add_error(column_errors, sanitized_file_name, "Columns>> が見つかりません。")
 
-            file_name = os.path.splitext(file.name)[0]
-            file_data[file_name] = measurement_data
+            graph_name = os.path.splitext(sanitized_file_name)[0]
+            file_data[graph_name] = measurement_data
 
         return file_data
--- backend/graph/views.py
+++ backend/graph/views.py
@@ -1,4 +1,3 @@
-from rest_framework.views import APIView
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from .models import FileData
